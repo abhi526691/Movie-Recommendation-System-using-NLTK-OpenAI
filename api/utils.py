aecac7acefbd63fdca2ae83ef527dc210433247a
Abhishek <53704940+abhi526691@users.noreply.github.com>
2023-11-13 16:26:17 -0500

    openai implemented

--- api/utils.py
+++ api/utils.py
@@ -1,64 +1,26 @@
+import openai
+import numpy as np
+# from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
-import ast  # For converting string representations of lists into actual lists
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-
-# Ensure that NLTK has the necessary data for processing
-nltk.download('punkt')
-nltk.download('stopwords')
-
-# Load the CSV file
-file_path = 'csv/final_dataset.csv'  # Update this path to where the CSV file is located
-data = pd.read_csv(file_path)
-
-# Function to convert string representations of lists into actual lists
-def parse_list_string(list_string):
-    try:
-        return ast.literal_eval(list_string)
-    except ValueError:
-        return []  # Return an empty list if there is a parsing error
-
-# Apply the parsing function to the list-like columns
-list_columns = ['genres', 'keywords', 'production_companies', 'cast']
-for column in list_columns:
-    data[column] = data[column].apply(parse_list_string)
-
-# Define the preprocessing function for text fields
-def preprocess_text(text):
-    # Tokenize the text
-    tokens = word_tokenize(text.lower())
-    # Remove stop words
-    stop_words = set(stopwords.words('english'))
-    # Return the preprocessed text
-    return " ".join([token for token in tokens if token not in stop_words])
-
-# Preprocess the overviews and taglines
-data['processed_overview'] = data['overview'].apply(preprocess_text)
-data['processed_tagline'] = data['tagline'].apply(preprocess_text)
-
-# Initialize TF-IDF Vectorizer for overviews
-tfidf_vectorizer_overview = TfidfVectorizer()
-# Fit and transform the processed overviews to create the feature matrix
-tfidf_matrix_overview = tfidf_vectorizer_overview.fit_transform(data['processed_overview'])
-
-# If you want to use taglines for recommendations, initialize another TF-IDF Vectorizer for taglines
-# tfidf_vectorizer_tagline = TfidfVectorizer()
-# tfidf_matrix_tagline = tfidf_vectorizer_tagline.fit_transform(data['processed_tagline'])
+from openai.embeddings_utils import cosine_similarity, get_embedding
 
 
-# Define the recommendation function
-def recommend_movies_based_on_prompt(prompt, data, tfidf_vectorizer, tfidf_matrix, top_n=5):
-    # Preprocess the prompt
-    preprocessed_prompt = preprocess_text(prompt)
-    # Transform the prompt into the same feature space as the overviews
-    prompt_tfidf_vector = tfidf_vectorizer.transform([preprocessed_prompt])
-    # Compute cosine similarity between the prompt and all movie overviews
-    cosine_similarities = cosine_similarity(prompt_tfidf_vector, tfidf_matrix)
-    # Get the top n similar movies
-    similar_movies_idx = cosine_similarities[0].argsort()[:-top_n-1:-1]
-    # Get the titles of the recommended movies
-    recommended_movies = data.iloc[similar_movies_idx]['title'].values
-    return recommended_movies
+class SupportingFunc:
+    def __init__(self):
+        openai.api_key = ''
+
+    def find_top_movies(self, query, df_movies, top_n=5):
+        product_embedding = get_embedding(
+            query,
+            engine="text-embedding-ada-002"
+        )
+
+        df_movies["embedding"] = df_movies.embedding.apply(eval).apply(np.array)
+        df_movies["similarity"] = df_movies.embedding.apply(
+            lambda x: cosine_similarity(x, product_embedding))
+        
+        sorted_movies = df_movies.sort_values(by='similarity', ascending=False)
+        return sorted_movies.head(top_n)
+
+
+
--- api/views.py
+++ api/views.py
@@ -3,28 +3,38 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import RecommendationSerializer
 from django.shortcuts import render
+from .utils import SupportingFunc
+import pandas as pd
 
-# Import the recommend_movies_based_on_prompt function from the utils.py file
-from .utils import recommend_movies_based_on_prompt, tfidf_vectorizer_overview, tfidf_matrix_overview, data
+# Load the movies DataFrame once and reuse it
+try:
+    df_movies = pd.read_csv('csv/movies_with_embedding.csv')
+except Exception as e:
+    df_movies = pd.DataFrame()
+    print(f"Error loading movies dataset: {e}")
 
-# This view will serve the HTML page
+# Instantiate SupportingFunc
+support_func = SupportingFunc()
+
+# View to serve the HTML page
 def recommendations_page(request):
     return render(request, 'index.html')
 
-
 class MovieRecommendationView(APIView):
     def get(self, request, format=None):
-        # Get the prompt from the request query parameters
         prompt = request.query_params.get('prompt', None)
         if prompt is None:
             return Response({'error': 'No prompt provided'}, status=status.HTTP_400_BAD_REQUEST)
-        
-        # Use the recommendation function from the script to get recommendations
-        recommendations = recommend_movies_based_on_prompt(prompt, data, tfidf_vectorizer_overview, tfidf_matrix_overview)
-        
+
+        if df_movies.empty:
+            return Response({'error': 'Movies dataset not loaded'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        # Find top 5 similar movies
+        top_movies = support_func.find_top_movies(prompt, df_movies)
+        print("top_movies", top_movies)
         # Serialize the recommendations
-        serializer = RecommendationSerializer(data={'recommendations': recommendations})
-        
+        serializer = RecommendationSerializer(data={'recommendations': top_movies['title'].tolist()})
+
         if serializer.is_valid():
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
